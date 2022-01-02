9583a89632cab1e3049ef37abf9727ec8f29e69a
Deyvis17 <deyvisjaime17@gmail.com>
2022-01-01 23:34:28 -0500

    🚩 add router edit task

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Board from "./views/Board.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "board",
+      component: Board,
+      children: [
+        {
+          path: "task/:id",
+          name: "task",
+          component: () => import("./views/Task.vue")
+        }
+      ]
+    }
+  ]
+});

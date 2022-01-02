ba857953faaf7dfeea42188b3b8f5701f67ca514
Deyvis17 <deyvisjaime17@gmail.com>
2022-01-01 23:32:26 -0500

    🚩 add vuex to project

---
+++ src/store.js
@@ -0,0 +1,67 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import defaultBoard from "./default-board";
+import { saveStatePlugin, uuid } from "./utils";
+
+Vue.use(Vuex);
+
+const board = JSON.parse(localStorage.getItem("board")) || defaultBoard;
+
+export default new Vuex.Store({
+  plugins: [saveStatePlugin],
+  state: {
+    board
+  },
+  getters: {
+    getTask(state) {
+      return id => {
+        for (const column of state.board.columns) {
+          for (const task of column.tasks) {
+            if (task.id === id) {
+              return task;
+            }
+          }
+        }
+      };
+    }
+  },
+  mutations: {
+    CREATE_TASK(state, { tasks, name }) {
+      tasks.push({
+        name,
+        id: uuid(),
+        description: ""
+      });
+    },
+    UPDATE_TASK(state, { task, key, value }) {
+      task[key] = value;
+      // Vue.set(task, key, value);
+    },
+    MOVE_TASK(state, { fromTasks, toTasks, fromTaskIndex, toTaskIndex }) {
+      const taskToMove = fromTasks.splice(fromTaskIndex, 1)[0];
+      toTasks.splice(toTaskIndex, 0, taskToMove);
+      // fromColumn.tasks.splice(fromIndex, 1);
+      // toColumn.tasks.push(task);
+    },
+    MOVE_COLUMN(state, { fromColumnIndex, toColumnIndex }) {
+      const columnList = state.board.columns;
+      const columnToMove = columnList.splice(fromColumnIndex, 1)[0];
+      columnList.splice(toColumnIndex, 0, columnToMove);
+    },
+    CREATE_COLUMN(state, { name }) {
+      state.board.columns.push({
+        name,
+        tasks: []
+      });
+    },
+    DELETE_TASK(state, { tasks, taskIndex }) {
+      tasks.splice(taskIndex, taskIndex + 1);
+    },
+    DELETE_COLUMN(state, { columnIndex }) {
+      state.board.columns.splice(columnIndex, 1);
+    },
+    IMPORT_DATA(state, { board }) {
+      state.board = board;
+    }
+  }
+});
